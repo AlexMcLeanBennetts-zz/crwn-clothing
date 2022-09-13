cb19e139f9ec0bc4a5a84bf5ba5d6f6d7849edec
Alex McLean <alex.mclean@bennetts.co.uk>
2022-09-13 15:34:12 +0100

    refactoring of directory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -32,23 +32,7 @@ const App = () => {
 
 
   return (
-    <div className="categories-container">
-
-      {categories.map(category => {
-        return (
-
-          <div className="category-container" key={category.id}>
-            <div className="background-image" style={{
-              backgroundImage: `url(${category.imageUrl})`,
-            }} />
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop now</p>
-            </div>
-          </div>
-        )
-      })}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
